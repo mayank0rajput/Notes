3d06b8f8597b422f2d809fe8796a9713cd6ad9b1
mayank <mayankrajput40558@gmail.com>
2023-08-11 18:14:30 +0530

    feat: added roomviewmodel

--- app/src/main/java/com/example/notes/MainActivity.kt
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewModel: NoteViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.viewModel = ViewModelProvider(this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(NoteViewModel::class.java)
+        viewModel.allNote.observe(this, Observer {
+            
+        })
     }
 }
